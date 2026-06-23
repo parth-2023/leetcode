ecb0deabcebd44c177a7520b9659f86a37364ddb
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-06-23 15:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (8.83%) - LeetSync

---
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int left=0; int right=colors.length-1;
+        int res=0;
+        while(left<right){
+            if(colors[left]!=colors[right]){
+                res=right-left;
+                break;
+            }
+            else right--;
+        }
+        right=colors.length-1;
+        while(left<right){
+            if(colors[left]!=colors[right]){
+                res=Math.max(res, right-left);
+                break;
+            }
+            left++;
+        }
+        return res;
+    }
+}
