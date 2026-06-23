ef917de8744014d01756133559c2f0b10ec4dd82
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-06-23 15:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (16.27%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,20 +1,27 @@
 class Solution {
-
     public boolean isTrionic(int[] nums) {
         int n = nums.length;
-        int i = 1;
-        while (i < n && nums[i - 1] < nums[i]) {
+        if (n < 4) return false;
+
+        int i = 0;
+
+        while (i + 1 < n && nums[i] < nums[i+1]) {
             i++;
         }
-        int p = i - 1;
-        while (i < n && nums[i - 1] > nums[i]) {
+
+        if (i == 0 || i == n - 1) return false;
+
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i+1]) {
             i++;
         }
-        int q = i - 1;
-        while (i < n && nums[i - 1] < nums[i]) {
+
+        if (i == p || i == n - 1) return false;
+
+        while (i + 1 < n && nums[i] < nums[i+1]) {
             i++;
         }
-        int flag = i - 1;
-        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+
+        return i == n - 1;
     }
 }
