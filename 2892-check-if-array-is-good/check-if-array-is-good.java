e6ead765d777e7687154c0adf9b4c404d41906c0
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-06-23 14:34:48 +0530

    Time: 1 ms (89.80%) | Memory: 44.8 MB (51.06%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        Set<Integer> list= new HashSet<>();
+        for(int i:nums){
+            if(i>=nums.length) return false;
+        }
+        int count=1;
+        for(int i:nums){
+            if(!list.contains(i)) list.add(i);
+            else if(i!=nums.length-1) return false;
+            else{
+                if(count==2) return false;
+                count++;
+            }
+        }
+        return true;
+    }
+}
