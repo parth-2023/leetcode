707e7820e1676b314cdede66f0f602044e65f943
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-07-12 12:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (72.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,9 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        int[]ways=new int[45];
+        int[] ways= new int[45];
         ways[0]=1;
         ways[1]=2;
-        for(int i=2; i<45; i++){
+        for(int i=2; i<n; i++){
             ways[i]=ways[i-1]+ways[i-2];
         }
         return ways[n-1];
