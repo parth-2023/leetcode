01beee208d6dffed63755ca8d65eb890f2bf6e78
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-07-12 10:30:38 +0530

    Time: 15 ms (70.43%) | Memory: 109 MB (5.06%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,10 +2,13 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet <Integer> set = new HashSet<>();
         for (int num : nums){
-            if(set.contains(num)){
+            // if(set.contains(num)){
+            //     return true;
+            // }
+            // set.add(num);
+            if(!set.add(num)){
                 return true;
             }
-            set.add(num);
         }
         return false;
     }
