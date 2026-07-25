a740b9d67c2af994805b9c67be954437a2c1a185
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-07-25 16:46:23 +0530

    Time: 1 ms (79.86%) | Memory: 43 MB (63.87%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -3,24 +3,16 @@ class Solution {
         int n= s.length();
         if(s.charAt(0)=='0'||n==0) return 0;
         if(n==1) return 1;
-        int d1=s.charAt(0)-'0';
-        int d2=s.charAt(1)-'0';
 
-        int[]dp= new int[n];
-        int num=d1*10+d2;
-        dp[0]= d1>0?1:0;
-        dp[1]= d2>0?1:0;
-        dp[1]+=num>=10 && num<=26?1:0;
+        int[]dp= new int[n+1];
+        dp[0]= 1;
+        dp[1]= 1;
 
-        for(int i=2;i<n; i++){
-            d1=s.charAt(i-1)-'0';
-            d2=s.charAt(i)-'0';
-
-            num=d1*10+d2;
-            int sum= d2>0?dp[i-1]:0;
-            sum+=num>=10 && num<=26?dp[i-2]:0;
-            dp[i]=sum;
+        for(int i=2;i<=n; i++){   
+            if(s.charAt(i-1)!='0') dp[i]=dp[i-1];
+            int num=(s.charAt(i-2)-'0')*10+(s.charAt(i-1)-'0');
+            dp[i]+=num>=10 && num<=26?dp[i-2]:0;
         }
-        return dp[n-1];
+        return dp[n];
     }
 }
