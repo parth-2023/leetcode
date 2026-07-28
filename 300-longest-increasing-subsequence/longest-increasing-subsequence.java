8d5c15e6cdf2d237d9729cc8ce85555177d8d159
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-07-28 09:33:37 +0530

    Time: 7 ms (79.77%) | Memory: 46.2 MB (42.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         List<Integer> list= new ArrayList<>();
-        
         for(int n:nums){
             if(list.isEmpty()||n>list.get(list.size()-1)){
                 list.add(n);
             }
             else{
-                list.set(Bin(list, n), n);
+                list.set(bin(list, n), n);
             }
         }
         return list.size();
     }
-    public static int Bin(List<Integer> nums, int num){
-        int left=0; int right=nums.size();
+    public static int bin(List<Integer> arr, int num){
+        int left=0; int right=arr.size()-1;
         while(left<=right){
-            int mid=(right+left)/2;
-            if(num>nums.get(mid)) left=mid+1;
-            else if(num<nums.get(mid)) right=mid-1;
+            int mid=(left+right)/2;
+            if(arr.get(mid)<num) left=mid+1;
+            else if(arr.get(mid)>num)right=mid-1;
             else return mid;
         }
         return left;
