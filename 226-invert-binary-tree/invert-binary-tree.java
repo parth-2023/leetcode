f31e927627373de5b6e51d2f5f84bffe9a8845c3
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-08-26 19:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.80%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -17,10 +17,8 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null) return null;
         TreeNode temp=root.left;
-        root.left=root.right;
-        root.right=temp;
-        invertTree(root.left);
-        invertTree(root.right);
+        root.left=invertTree(root.right);
+        root.right=invertTree(temp);;
         return root;
     }
 }
