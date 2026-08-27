7d1e831f25def94751cffac4f4429fd35bb73f05
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-08-27 21:52:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (52.68%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,6 +19,7 @@ class Solution {
         if(p!=null&&q!=null&&p.val==q.val){
             return isSameTree(p.left, q.left)&&isSameTree(p.right, q.right);
         }
-        return false;
+        else
+            return false;
     }
 }
