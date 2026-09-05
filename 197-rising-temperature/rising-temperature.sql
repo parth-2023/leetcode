ee25bb077b6dbd74dea6ec726505ce356307a032
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-09-05 10:30:42 +0530

    Time: 517 ms (55.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT today.id FROM Weather today CROSS JOIN Weather yesterday WHERE datediff(today.recordDate, yesterday.recordDate)=1 AND today.temperature>yesterday.temperature
