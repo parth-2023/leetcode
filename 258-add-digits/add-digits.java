7a0a99820b6395efe67add2db597a40b7c708d13
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-09-05 22:38:58 +0530

    Time: 1 ms (98.09%) | Memory: 42.5 MB (62.96%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,16 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        while(num/10!=0){
-            num=sum(num);
-        }
-        return num;
-    }
-    public int sum(int num){
-        int sum=0;
-        while(num>0){
-            sum+=num%10;
-            num/=10;
-        }
-        return sum;
+        if(num==0) return 0;
+        else if(num%9==0) return 9;
+        return num%9;
     }
 }
