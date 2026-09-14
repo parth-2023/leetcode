819a4a95ee0a8fe092f604bf886e2a1f2c5e7b20
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-09-14 12:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (10.63%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,11 +1,11 @@
 class Solution {
     public String triangleType(int[] nums) {
-        int max = Math.max(nums[0], Math.max(nums[1], nums[2]));
-        int min = Math.min(nums[0], Math.min(nums[1], nums[2]));
-        int mid = nums[0] + nums[1] + nums[2] - max - min;
-        if (min + mid <= max) return "none"; 
-        if(nums[0]!=nums[1]&&nums[1]!=nums[2]&&nums[0]!=nums[2]) return "scalene";
-        if(nums[0]==nums[1]&&nums[0]==nums[2]&&nums[1]==nums[2]) return "equilateral";
-        return "isosceles";
+        int a=nums[0];
+        int b=nums[1];
+        int c=nums[2];
+        if(a+b<=c || b+c<=a || a+c<=b)return "none";
+        if(a==b && b==c)return "equilateral";
+        if(a==b || b==c || c==a)return "isosceles";
+        return "scalene";
     }
 }
