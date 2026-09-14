317db875c1e3dea5f5bf9e6aa55eea30ab85a20d
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-09-14 12:18:03 +0530

    Time: 1 ms (93.12%) | Memory: 42.5 MB (46.70%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -4,9 +4,8 @@ class Solution {
         for(int i=1; i<=n; i++){
             if(i%m!=0){
                 notdiv+=i;
-                continue;
             }
-            div+=i;
+            else div+=i;
         }
         return notdiv-div;
     }
