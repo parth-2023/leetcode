7b7b121a0f03bee7bbe54b699682977397c7af14
Parth Gupta <parth.gupta2023c@vitstudent.ac.in>
2026-09-14 12:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.73%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,11 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        char[] c= s.toCharArray();
-        for(int i=0; i<s.length(); i++){
-            if(Character.isUpperCase(c[i])){
-                c[i]=Character.toLowerCase(c[i]);
-            }
-        }
-        return new String(c);
+        return s.toLowerCase();
     }
 }
